7347f21c3ecdb411f851bc66070bd24645724b48
lenysi4ka25 <lenysi4ka@mail.ru>
2022-09-21 20:25:57 +0300

    Не совсем поняла материал!!!

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задача 1, Задача 2
+        System.out.println(" ");
+        System.out.println("Задача 1, Задача 2");
+        System.out.println(" ");
+        int[] num = new int[3];
+        num[0] = 1;
+        num[1] = 2;
+        num[2] = 3;
+        double[] nums = {1.57, 7.654, 9.986};
+        nums[0] = 1.57;
+        nums[1] = 7.654;
+        nums[2] = 9.986;
+        int[] book = {75, 560, 999};
+        book[0] = 75;
+        book[1] = 560;
+        book[2] = 999;
+        System.out.print(num[0] + ", " + num[1] + ", " + num[2] + " ");
+        System.out.println(" ");
+        System.out.print(nums[0] + ", " + nums[1] + ", " + nums[2] + " ");
+        System.out.println(" ");
+        System.out.print(book[0] + ", " + book[1] + ", " + book[2] + " ");
+        System.out.println(" ");
+        //Задача 3
+        System.out.println(" ");
+        System.out.println("Задача 3");
+        System.out.println(" ");
+        System.out.print(num[2] + ", " + num[1] + ", " + num[0] + " ");
+        System.out.println(" ");
+        System.out.print(nums[2] + ", " + nums[1] + ", " + nums[0] + " ");
+        System.out.println(" ");
+        System.out.print(book[2] + ", " + book[1] + ", " + book[0] + " ");
+        System.out.println(" ");
+        //Задача 4
+        System.out.println(" ");
+        System.out.println("Задача 4");
+        System.out.println(" ");
+        int[] num1 = {1, 2, 3};
+        for (int u = 0; u < num1.length; u++)
+        if (num1 [u] % 2 != 0)
+        System.out.println(num1[u] + 1);
+    }
+    }
